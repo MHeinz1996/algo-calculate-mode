4c757f5a6c243b015e72dad6bbdd803ae2eebaf3
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-31 00:54:57 -0500

    Completed algo

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,32 @@
-const calculateMode = () => {
+exports.calculateMode = (arr) => {
+    myObj = {};
 
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] in myObj) {
+            myObj[arr[i]]+=1;
+        } else {
+            myObj[arr[i]] = 1;
+        }
+    }
+
+    let sortable = [];
+    for (let item in myObj) {
+        sortable.push([item, myObj[item]]);
+    }
+    
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+
+    let occurrence = +(sortable[0][1]);
+
+    let mode = [];
+
+    for(let i=0; i<sortable.length; i++) {
+        if(+(sortable[i][1]) >= occurrence) {
+            mode.push(+sortable[i][0]);
+        }
+    }
+
+    return mode;
 }
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,26 @@
-// Write your own unit tests here!
+var mode = require("./calculateMode");
+
+let resutl_1 = mode.calculateMode([1, 2, 3, 3, 4, 4, 4, 5, 5, 6]);
+console.log(resutl_1[0][0] === JSON.stringify[4]);
+
+let resutl_2 = mode.calculateMode([1,2,3,3])
+console.log(resutl_2[0][0] === JSON.stringify[3]);
+
+let resutl_3 = mode.calculateMode([4.5, 0, 0])
+console.log(resutl_2[0][0] === JSON.stringify[0]);
+
+let resutl_4 = mode.calculateMode([1.5, -1, 1, 1.5])
+console.log(resutl_4[0][0] === JSON.stringify[1.5]);
+
+let resutl_5 = mode.calculateMode([1,1,2,2])
+console.log(resutl_5[0][0] === JSON.stringify[1]);
+console.log(resutl_5[0][1] === JSON.stringify[1]);
+
+let resutl_6 = mode.calculateMode([1,2,3])
+console.log(resutl_5[0][0] === JSON.stringify[1]);
+console.log(resutl_5[0][1] === JSON.stringify[2]);
+console.log(resutl_5[0][3] === JSON.stringify[3]);
+
+// console.log(mode.calculate_mode(["who", "what", "where", "who"]) === ["who"])
+let resutl_7 = mode.calculateMode(["who", "what", "where", "who"])
+console.log(resutl_5[0][0] === JSON.stringify['who']);
